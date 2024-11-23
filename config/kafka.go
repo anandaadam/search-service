1af33f5ede7de1102182178c712ff18521765da8
Adam Ananda Santoso <adamananda02@gmail.com>
2024-11-23 21:42:56 +0700

    feat: add kafka config

---
+++ config/kafka.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func KafkaConfig() *kafka.ConfigMap {
+	return &kafka.ConfigMap{
+		"bootstrap.servers": fmt.Sprintf("%s:%s", os.Getenv("KAFKA_HOST"), os.Getenv("KAFKA_PORT")),
+		"group.id":          os.Getenv("KAFKA_GROUP"),
+		"auto.offset.reset": "earliest",
+	}
+}
